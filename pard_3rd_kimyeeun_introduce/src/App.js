806d738cdc2da55d0abf96bf283a87dd69c0663e
KKIMYENN <yenny031003@gmail.com>
2024-03-17 02:47:23 +0900

    과제1

--- pard_3rd_kimyeeun_introduce/src/App.js
+++ pard_3rd_kimyeeun_introduce/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="First">
+          <div className="Title">MY NAME</div>
+          <div className="Title">김예은</div>
+          <div className="SubTitle">#PARD_3rd_WEB #PARD</div>
+        </div>
+
+        <div className="Second">
+          <div className="AboutMe">ABOUT ME</div>
+          <div className="Container">
+            <div className="Main1">
+              <span>#Age</span>
+            </div>
+            <div className="Main2">
+              <span>#I'm from</span>
+            </div>
+            <div className="Main3">
+              <span>#I love</span>
+            </div>
+            <div className="Main4">
+              <span>#RC</span>
+            </div>
+            <div className="Main5">
+              <span>#MBTI</span>
+            </div>
+            <div className="Main6">
+              <span>#Sports</span>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
